cd184bfbc9f079fe057dfdff078e8bfc014c1864
IlyaLevchenko04 <109174985+IlyaLevchenko04@users.noreply.github.com>
2023-03-23 10:30:34 +0000

    Deploying to gh-pages from @ IlyaLevchenko04/goit-react-hw-05-movies@db1bf2f055eed043baf15dfeb00e654aac812146 🚀

---
+++ static/js/869.6785d71d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[869],{869:function(e,t,n){n.r(t);var r=n(87),s=n(184),c=n(87).Outlet;t.default=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsxs)("nav",{children:[(0,s.jsx)(r.Link,{to:"/",children:"Home"}),(0,s.jsx)(r.Link,{to:"/movies",children:"Movies"})]}),(0,s.jsx)(c,{})]})}}}]);
+//# sourceMappingURL=869.6785d71d.chunk.js.map
